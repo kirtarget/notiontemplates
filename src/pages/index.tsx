b7666e8c048effe1829b7181530004858c5d858e
kirtarget <sheldongriffiths1@gmail.com>
2023-01-09 21:39:18 +0300

    Added "loading" state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,13 +19,17 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen w-full flex-col bg-slate-100">
         <Navbar />
-        {hasMounted && (
+        {hasMounted && getNames.isFetching && (
+          <p className="px-4 font-bold text-slate-600">Загрузка...</p>
+        )}
+        {hasMounted && getNames.isSuccess && (
           <div className="flex flex-wrap">
             {getNames.data?.map((item) => {
               const title = item.title?.rich_text?.[0]?.plain_text ?? "";
 
               const description =
                 item.description?.rich_text?.[0]?.plain_text ?? "";
+
               const instruction = item.instruction?.url ?? "";
 
               const toPublish = item.toPublish?.checkbox;
@@ -37,6 +41,7 @@ const Home: NextPage = () => {
               if (!toPublish) {
                 return null;
               }
+
               return (
                 <Item
                   key={Math.random()}
@@ -57,27 +62,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-// const AuthShowcase: React.FC = () => {
-//   const { data: sessionData } = useSession();
-
-//   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-//     undefined, // no input
-//     { enabled: sessionData?.user !== undefined }
-//   );
-
-//   return (
-//     <div className="flex flex-col items-center justify-center gap-4">
-//       <p className="text-center text-2xl text-white">
-//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-//         {secretMessage && <span> - {secretMessage}</span>}
-//       </p>
-//       <button
-//         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-//         onClick={sessionData ? () => signOut() : () => signIn()}
-//       >
-//         {sessionData ? "Sign out" : "Sign in"}
-//       </button>
-//     </div>
-//   );
-// };
